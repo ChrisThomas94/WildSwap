f8a962b1bb85df9469c977ac3ddc694c0eb3bf78
Chris Thomas <chris.thomas94@hotmail.co.uk>
2016-02-05 18:52:04 +0000

    long lat auto pop

--- app/src/main/java/scot/wildcamping/wildscotland/AddSite.java
+++ app/src/main/java/scot/wildcamping/wildscotland/AddSite.java
@@ -10,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RatingBar;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -25,17 +27,49 @@ import java.util.Map;
 
 public class AddSite extends Activity {
 
+    EditText Lat;
+    EditText Long;
+    ImageButton addImage;
+    ImageButton addFeature;
+    TextInputLayout title;
+    TextInputLayout description;
+    RatingBar rating;
+    Button confirmCreation;
+    Button cancelCreation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_site);
+
+        Lat =(EditText)findViewById(R.id.Lat);
+        Long = (EditText)findViewById(R.id.Long);
+        addImage = (ImageButton)findViewById(R.id.addImage);
+        addFeature = (ImageButton)findViewById(R.id.addFeature);
+        title = (TextInputLayout)findViewById(R.id.addTitle);
+        description = (TextInputLayout)findViewById(R.id.addDescription);
+        rating = (RatingBar)findViewById(R.id.ratingBar);
+        confirmCreation = (Button)findViewById(R.id.confirmCreation);
+        cancelCreation = (Button)findViewById(R.id.cancelCreation);
 
         Bundle extras = getIntent().getExtras();
         if(extras != null)
         {
             double latitude = extras.getDouble("latitude");
             double longitude = extras.getDouble("longitude");
+
+            String lat = Double.toString(latitude);
+            String lon = Double.toString(longitude);
+
+            Lat.setText(lat);
+            Long.setText(lon);
+
         }
+
+
+
+
+
 
     }
 
